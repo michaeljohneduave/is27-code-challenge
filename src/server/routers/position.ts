628751dde50375afc45a4c9a53c5cc003fb2d7a9
Michael Eduave <michaeljohneduave@gmail.com>
2023-05-01 21:46:37 -0700

    Add position types rpc

--- src/server/routers/position.ts
+++ src/server/routers/position.ts
@@ -1,20 +1,41 @@
 import { z } from "zod";
 import { procedure, router } from "../trpc";
 import { prisma } from "@/server/prisma";
-import { pageObject } from "@/utils/trpc";
+import { Prisma } from "@prisma/client";
 
 export const positionRouter = router({
-  list: procedure.input(z.object(pageObject)).query(async function ({ input }) {
-    const { limit, page } = input;
+  types: procedure.query(async function () {
+    const types = await prisma.position.findMany({
+      select: {
+        title: true,
+      },
+      distinct: ['title']
+    })
+
+    return types;
+  }),
+
+  list: procedure.input(z.object({
+    title: z.string(),
+    page: z.number().min(1),
+    limit: z.number().min(1).max(100),
+  })).query(async function ({ input }) {
+    const { limit, page, title } = input;
+    const where: Prisma.PositionWhereInput = {};
+
+    if (title) {
+      where.title = title;
+    }
 
     const positions = await prisma.position.findMany({
+      where,
       take: limit,
       skip: (page - 1) * limit,
       orderBy: {
         createdAt: "desc",
       },
       include: {
-        Employee: {
+        employee: {
           include: {
             Tenure: {
               select: {
@@ -43,6 +64,18 @@ export const positionRouter = router({
         where: {
           id,
         },
+        include: {
+          employee: {
+            include: {
+              Tenure: {
+                select: {
+                  startDate: true,
+                  endDate: true,
+                }
+              }
+            }
+          }
+        }
       });
     }),
 });
