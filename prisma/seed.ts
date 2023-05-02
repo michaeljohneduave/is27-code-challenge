ec4f90c3d485ab8f315d9828160f627ef5d1de98
Michael Eduave <michaeljohneduave@gmail.com>
2023-05-01 21:45:33 -0700

    Add react avatar package Add superjson to serialize non primitives

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,49 +1,25 @@
 import { faker } from "@faker-js/faker";
 import { Employee, Position, Prisma, PrismaClient } from "@prisma/client";
 import https from "https";
+import { genConfig } from "react-nice-avatar";
 
 const prisma = new PrismaClient();
 
-const roles: string[] = [
-  "Director",
-  "Senior Manager",
-  "Manager",
-  "Senior Developer",
-  "Junior Developer",
+type Role = {
+  role: string;
+  level: number;
+};
+
+const roles: Role[] = [
+  { role: "Director", level: 0 },
+  { role: "Senior Manager", level: 1 },
+  { role: "Manager", level: 2 },
+  { role: "Senior Developer", level: 3 },
+  { role: "Junior Developer", level: 4 },
 ];
 
-function get(url: string, callback: (arg0: any | null, arg1: string | null) => void) {
-  https
-    .get(url, (res) => {
-      if (
-        res.statusCode &&
-        res.statusCode >= 300 &&
-        res.statusCode < 400 &&
-        res.headers.location
-      ) {
-        get(res.headers.location, callback);
-      } else {
-        callback(null, url);
-      }
-    })
-    .on("error", (error: any) => {
-      callback(error, null);
-    });
-}
 
-function getImage(): Promise<string | null> {
-  return new Promise((resolve, reject) => {
-    get("https://xsgames.co/randomusers/avatar.php?g=pixel", (error, url) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(url);
-      }
-    })
-  });
-}
-
-async function createPositions(role: string, posCount: number): Promise<void> {
+async function createPositions(role: Role, posCount: number): Promise<void> {
   const vacantCount = Math.floor(posCount * 0.2);
   let employees = 0;
   let positions = 0;
@@ -51,26 +27,29 @@ async function createPositions(role: string, posCount: number): Promise<void> {
   for (let i = 0; i < posCount - vacantCount; i += 1) {
     const positionRec = await prisma.position.create({
       data: {
-        title: role,
-        Employee: {
+        title: role.role,
+        level: role.level,
+        employee: {
           create: {
             name: faker.name.fullName({}),
-            profileImage: await getImage(),
+            avatar: JSON.stringify(genConfig()),
           },
         },
       },
       select: {
-        Employee: true,
+        employee: true,
       },
     });
 
-    await prisma.tenure.create({
-      data: {
-        employeeId: positionRec.Employee[0].id,
-        startDate: new Date(),
-        position: role,
-      },
-    });
+    if (positionRec.employee?.id) {
+      await prisma.tenure.create({
+        data: {
+          employeeId: positionRec.employee?.id,
+          startDate: new Date(),
+          position: role.role,
+        },
+      });
+    }
 
     employees += 1;
   }
@@ -78,15 +57,16 @@ async function createPositions(role: string, posCount: number): Promise<void> {
   for (let i = 0; i < vacantCount; i += 1) {
     const positionRec = await prisma.position.create({
       data: {
-        title: role,
+        title: role.role,
+        level: role.level,
       },
     });
 
     positions += 1;
   }
 
-  console.log(`Created ${employees} filled positions for ${role}`);
-  console.log(`Created ${positions} vacant positions for ${role}`);
+  console.log(`Created ${employees} filled positions for ${role.role}`);
+  console.log(`Created ${positions} vacant positions for ${role.role}`);
 }
 
 async function main() {
